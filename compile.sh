5d9a4611aca83281ec37e48f9bd744ab03f7c6c9
Brian Huffman <huffman@galois.com>
2021-01-09 10:21:54 -0800

    Change from .ca65 to more standard .a65 file extension.
    The ca65 assembler recognizes the "a65" extension as identifying
    assembler files.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-ca65 -t c64 -o drmario.o drmario.ca65
+ca65 -t nes -o drmario.o drmario.a65
 ld65 --lib c64.lib -C rom32k.cfg -o drmario.prg drmario.o
 ld65 -C drmario.cfg -o drmario.nes drmario.o
 diff drmario.prg drmario.prg.gold
