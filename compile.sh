5238001aaf97ca330fc4fbf78125e715d73d5b25
Brian Huffman <huffman@galois.com>
2021-01-09 10:44:33 -0800

    Replace drmario.prg.gold with drmario.nes.gold.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
 ca65 -t nes -o drmario.o drmario.a65
-ld65 --lib c64.lib -C rom32k.cfg -o drmario.prg drmario.o
 ld65 -C drmario.cfg -o drmario.nes drmario.o
-diff drmario.prg drmario.prg.gold
+diff drmario.nes drmario.nes.gold
