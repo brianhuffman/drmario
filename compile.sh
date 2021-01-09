18850bfa3f8a376281127eb8efbf247e7bfcc036
Brian Huffman <huffman@galois.com>
2021-01-09 10:16:57 -0800

    New .cfg file and .segment declarations to go with it.
    This lets us generate an entire .nes ROM image, complete with header.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 ca65 -t c64 -o drmario.o drmario.ca65
-ld65 --lib c64.lib -C rom32k.cfg --start-addr \$8000 -o drmario.rom drmario.o
-diff drmario.rom drmario.prg.gold
+ld65 --lib c64.lib -C rom32k.cfg -o drmario.prg drmario.o
+ld65 -C drmario.cfg -o drmario.nes drmario.o
+diff drmario.prg drmario.prg.gold
