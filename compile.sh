84376a44593e30ff33872371c9adc52d7cdf2529
Brian Huffman <huffman@galois.com>
2021-01-09 10:45:53 -0800

    Add drmario_a.a65 with commands to build and test REV A.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
 ca65 -t nes -o drmario.o drmario.a65
+ca65 -t nes -o drmario_a.o drmario_a.a65
 ld65 -C drmario.cfg -o drmario.nes drmario.o
+ld65 -C drmario.cfg -o drmario_a.nes drmario_a.o
 diff drmario.nes drmario.nes.gold
+diff drmario_a.nes drmario_a.nes.gold
