b62a0af0f5f70446e17495f4e887ba089d222353
Brian Huffman <huffman@galois.com>
2022-02-08 19:03:39 -0800

    Update compile script to check md5 hashes of expected output.

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@ ca65 -t nes -o drmario.o drmario.a65
 ca65 -t nes -o drmario_a.o drmario_a.a65
 ld65 -C drmario.cfg -o drmario.nes drmario.o
 ld65 -C drmario.cfg -o drmario_a.nes drmario_a.o
-diff drmario.nes drmario.nes.gold
-diff drmario_a.nes drmario_a.nes.gold
+md5sum -c drmario.nes.md5
+md5sum -c drmario_a.nes.md5
